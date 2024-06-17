359953ec4080f1e5d6253cdbb63defe9e57da9a5
0KeTeR0 <louis.demeocq@gmail.com>
2024-06-17 08:26:41 +0200

    [ HardcodedForecastProvider.java ] Ajout de nouvelles entrées pour avoir chaque type de météo

--- app/src/main/java/com/example/tp2devmobileapplication_meteo/HardcodedForecastProvider.java
+++ app/src/main/java/com/example/tp2devmobileapplication_meteo/HardcodedForecastProvider.java
@@ -9,11 +9,18 @@ public class HardcodedForecastProvider implements IForecastProvider {
     public WeatherForecast getForecast(Location location) {
         WeatherForecast forecast = new WeatherForecast();
 
-        // Ajout de prévisions "en dur"
-        forecast.addForecast(new Weather(0, 22, WeatherCodes.CLEAR_SKY, 15, 70, 10, "N", 0));
-        forecast.addForecast(new Weather(0, 23, WeatherCodes.PARTIAL_CLOUDED, 14, 72, 12, "NE", 0));
-        forecast.addForecast(new Weather(1, 0, WeatherCodes.FOGGY_CLOUDED, 13, 80, 5, "E", 0));
-        forecast.addForecast(new Weather(1, 1, WeatherCodes.SMALL_RAIN, 12, 85, 8, "SE", 2));
+        // jour 0, heure 22
+        forecast.addForecast(new Weather(0, 22, WeatherCodes.CLEAR_SKY, 25, 60, 10, "N", 0));
+        // jour 0, heure 23
+        forecast.addForecast(new Weather(0, 23, WeatherCodes.PARTIAL_CLOUDED, 22, 65, 8, "NE", 0));
+        // jour 1, heure 0
+        forecast.addForecast(new Weather(1, 0, WeatherCodes.FOGGY_CLOUDED, 18, 70, 5, "E", 0));
+        // jour 1, heure 1
+        forecast.addForecast(new Weather(1, 1, WeatherCodes.SMALL_RAIN, 17, 85, 12, "SE", 1));
+
+        forecast.addForecast(new Weather(1, 2, WeatherCodes.HEAVY_RAIN, 15, 90, 15, "S", 5));
+        forecast.addForecast(new Weather(1, 3, WeatherCodes.SNOW, 0, 95, 20, "SW", 10));
+        forecast.addForecast(new Weather(1, 4, WeatherCodes.THUNDERSTORM, 12, 80, 25, "W", 7));
 
         return forecast;
     }
